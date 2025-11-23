f3d2ba76d7b4c6feb14f3a192a28375a9604c6ca
Rakshith-Codr-77 <rakshitha24cs@rnsit.ac.in>
2025-11-23 20:23:04 +0530

    added toml file

--- backend/server.js
+++ backend/server.js
@@ -17,10 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended: true}));
-app.use(cors({
-    origin: "https://blog-application-full-stack.onrender.com/api/v1",
-    credentials:true
-}))
+app.use(cors())
 
 const __dirname = path.resolve();
 
